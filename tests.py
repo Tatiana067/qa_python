0a14bf0ad6a1ee9499035572c770d2e342246575
Tatiana Aseeva <aseeva403@yandex.ru>
2026-09-08 14:52:08 +0300

    The tests are written for four methods of the BooksCollector application

--- tests.py
+++ tests.py
@@ -1,24 +1,66 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_init_books_genre_is_empty_dictionary(self):
         collector = BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert collector.books_genre == {}
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_init_favorites_is_empty_list(self):
+        collector = BooksCollector()
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert collector.favorites == []
+
+    def test_init_genre_contains_available_genres(self):
+        collector = BooksCollector()
+        available_genres = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+
+        assert collector.get_genres == available_genres
+
+    def test_init_genre_age_rating_contains_restricted_genres(self):
+        collector = BooksCollector()
+        restricted_genres = ['Ужасы', 'Детективы']
+
+        assert collector.get_genre_age_rating == restricted_genres
+
+    @pytest.mark.parametrize('name', ['','a','a*40','a*41'])
+    def test_add_new_book_name_book_title_contains_from_1_to_40_characters(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        
+        if 0 < len(name) < 41:
+            assert name in collector.get_books_genre()
+        else:
+            assert name not in collector.get_books_genre()
+
+    def test_add_new_book_can_be_added_only_once(self):
+        collector = BooksCollector()
+        collector.name = 'Кто украл лопату у кота'
+        collector.add_new_book('Кто украл лопату у кота')
+
+        assert len(collector.get_books_rating()) == 1
+
+    def test_set_book_genre_determines_the_genre_of_the_book(self):
+        collector = BooksCollector()
+        name = 'Кто украл лопату у кота'
+
+        collector.add_new_book(name)
+        collector.set_book_genre(name, 'Детективы')
+
+        assert collector.get_book_genre(name) == 'Детективы'
+
+    def test_get_book_genre_determine_the_genre_based_on_the_books_title(self):
+        collector = BooksCollector()
+        name = 'Кто украл лопату у кота'
+        book_genre = 'Детективы'
+
+        collector.add_new_book(name)
+        collector.set_book_genre(name, book_genre)
+
+        assert collector.get_book_genre(name) == book_genre
+
+
+        
+    
